22f30c5ab1b7f3b13f1895210c28fb0df6933dd1
ruyi <tan_ruyi@hotmail.com>
2022-09-10 16:59:58 +0800

    restricted the area where the character can move horizontally

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
 "use strict"
+
+/* =========================================
 // VARIABLES
+========================================= */
 
 const character1 = document.getElementById("character1");
 
 let character1Left = parseInt(window.getComputedStyle(character1).getPropertyValue("left"));
 
+/* =========================================
+// CALLBACK FUNCTIONS FOR EVENT LISTENERS
+========================================= */
 
+// moves left by 15px
 function moveCharLeft() {
-    character1Left -= 15;
-    character1.style.left = character1Left + 'px';
+    if (character1Left > 15) { // 15 is the current width of the character
+        character1Left -= 15; 
+        character1.style.left = character1Left + 'px';
+    }
 }
 
+// moves right by 15px
 function moveCharRight() {
-    character1Left += 15;
-    character1.style.left = character1Left + 'px';
+    if (character1Left < 1075) { // 1075 is current width of seesaw minus current width of character
+        character1Left += 15; 
+        character1.style.left = character1Left + 'px';
+    }
 }
 
 function control(e) {
@@ -26,5 +38,9 @@ function control(e) {
         moveCharRight();
     }
 }
+
+/* =========================================
+// EVENT LISTENERS
+========================================= */
 
 document.addEventListener("keydown", control);
